bcba3e2a96d1e8d5ba3022cbd127fd98d92848f7
Nino Lin <nino_lin@trendmicro.com>
2021-08-28 21:38:51 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
+const db = require('./models')
+const passport = require('./config/passport')
+const methodOverride = require('method-override')
+const app = express()
+const port = process.env.PORT || 3000
+app.use('/upload', express.static(__dirname + '/upload'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+app.use((req, res, next) => {
+    res.locals.success_messages = req.flash('success_messages')
+    res.locals.error_messages = req.flash('error_messages')
+    res.locals.user = req.user
+    next()
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+require('./routes')(app, passport)
